cf9e63115d30c635d12c187bc43909ec4cabef93
Nyah <nmukassa@gmail.com>
2024-10-27 17:02:56 +0000

    Github auth, Tailwind config, Custom Fonts, Favicon

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+function layout({ children }: Readonly<LayoutProps>) {
+    return ( 
+        <main className="font-work-sans">
+            <Navbar />
+            {children}
+        </main>
+     );
+}
+
+export default layout;
